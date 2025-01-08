dca48f5e18472084c5dee8da2ed049f1b4159e63
Yahya Mohamed <yahya.hm750@gmail.com>
2025-01-08 23:40:39 +0000

    fix data format posted to the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,23 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e))
 document.getElementById("submit").addEventListener("click", e => postForm(e))
 
+function processOptions(form){
+    let optArray = []
+
+    for (let entry of form.entries()){
+        if (entry[0] === "options"){
+            optArray.push(entry[1])
+        }
+    }
+    form.delete("options")
+
+    form.append("options", optArray.join())
+
+    return form
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"))
+    const form = processOptions(new FormData(document.getElementById("checksform")))
 
     const response = await fetch(API_URL, {
                                 method: "POST",
@@ -31,7 +46,7 @@ function displayErrors(data){
     } else{
         results = `<div>Total Errors: <span class="error_count>${data.total_errors}</span></div>`
         for (let error of data.error_list){
-            results += `<div>At line <span class="line">${error.line}</span>, `
+            results += `<div>At line <span class="line">${error.line}</span> `
             results += `column <span class="column">${error.col}</span></div>`
             results += `<div class="error">${error.error}</div>`
         }
